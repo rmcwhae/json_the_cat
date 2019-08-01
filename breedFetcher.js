8e9a13428942ea8e7ace85550c60a42acda0429f
Russell McWhae <russell.mcwhae@gmail.com>
2019-08-01 15:12:41 +0000

    Completed assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,10 +2,19 @@ const request = require('request');
 let input = process.argv[2];
 
 
-const felineFetch = function(breed) {
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+const felineFetch = function(query) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + query, (error, response, body) => {
+    if (error) {
+      return console.log('Request failed: ', error);
+    }
     const data = JSON.parse(body);
-    console.log(data[0].description);
+    let breed = data[0];
+    if (breed) {
+      console.log(data[0].description);
+    } else {
+      console.log('Breed "' + query + '" not found');
+    }
+      
   });
 };
 
